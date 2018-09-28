3ac2114a9931f8d76322e0e4516b57303e9f31c4
julenb <21701719@live.uem.es>
2018-09-28 16:43:59 +0200

    Magnitudes

--- src/LectorCSV.java
+++ src/LectorCSV.java
@@ -2,17 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class LectorCSV {
 
-    private static String crearHTML(LinkedList<String[]> datos) {
-        String html = "<html\n><head>\n</head>\n<body>\n<table>";
+    private static String crearHTML(LinkedList<String[]> datos, HashMap<Integer, String> magnitudes) {
+        String html = "<html\n><head>\n<meta charset='UTF-8'></head>\n<body>\n<table>";
         for (int x = 0; x < datos.size(); x++) {
             html += "\n<tr>";
             for (int i = 0; i < datos.get(x).length - 1; i++) {
                 if (!(datos.get(x)[i].charAt(0) == 'V') && !datos.get(x)[i].equalsIgnoreCase("n") &&
-                        (i > 1) && (i != 4) && !((i >= 5) && (i <= 7)) && (i < 47))
+                        (i > 1) && (i != 4) && (i != 3) && !((i >= 5) && (i <= 7)) && (i < 47))
                     html += "\n<td>" + datos.get(x)[i] + "</td>";
                 else if (i == 5) {
                     if (x > 0) {
@@ -20,6 +21,13 @@ public class LectorCSV {
                     } else {
                         html += "<td>FECHA</td>";
                     }
+                } else if (i == 3) {
+                        try{
+                            html += "<td>" + magnitudes.get(Integer.parseInt(datos.get(x)[i])) + "</td>";
+                        } catch (NumberFormatException e){
+                            html += "\n<td>" + datos.get(x)[i] + "</td>";
+                        }
+
                 }
 
             }
@@ -43,16 +51,22 @@ public class LectorCSV {
 
     public static void main(String[] args) {
         String strLinea;
-        LinkedList<String[]> tabla = new LinkedList<>();
+        LinkedList<String[]> tablaDatos = new LinkedList<>();
+        HashMap<Integer, String> tablaMagnitudes = new HashMap<>();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("horario.csv"));
-            while ((strLinea = bufferedReader.readLine()) != null) {
-                tabla.add(strLinea.split(";"));
+            BufferedReader bufferDatos = new BufferedReader(new FileReader("horario.csv"));
+            BufferedReader bufferMagnitudes = new BufferedReader(new FileReader("magnitudes.csv"));
+            while ((strLinea = bufferDatos.readLine()) != null) {
+                tablaDatos.add(strLinea.split(";"));
+            }
+            while ((strLinea = bufferMagnitudes.readLine()) != null) {
+                String[] magnitud = strLinea.split(";");
+                tablaMagnitudes.put(Integer.parseInt(magnitud[0]), magnitud[1]);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        generarFichero("./", crearHTML(tabla));
+        generarFichero("./", crearHTML(tablaDatos, tablaMagnitudes));
     }
 
 }
