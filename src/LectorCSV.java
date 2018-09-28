c6f51ae3bdf672c0c1cd38bab3779494e959bd7b
julenb <21701719@live.uem.es>
2018-09-28 17:16:34 +0200

    Mejora

--- src/LectorCSV.java
+++ src/LectorCSV.java
@@ -12,20 +12,19 @@ public class LectorCSV {
                 "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\">\n" +
                 "</head>\n<body>\n<table>\n<thead>";
         for (int x = 0; x < datos.size(); x++) {
-            if (x==1){
-                html+="</thead><tbody>";
+            if (x == 1) {
+                html += "</thead><tbody>";
             }
             html += "\n<tr>";
             for (int i = 0; i < datos.get(x).length - 1; i++) {
                 if (!(datos.get(x)[i].charAt(0) == 'V') && !datos.get(x)[i].equalsIgnoreCase("n") &&
-                        (i > 2) && (i != 3)&& (i!=4) && !((i >= 5) && (i <= 7)) && (i < 47)){
-                    if (x>0){
+                        i > 2 && i != 3 && i != 4 && !(i <= 7) && i < 47) {
+                    if (x > 0) {
                         html += "\n<td>" + datos.get(x)[i] + "</td>";
                     } else {
                         html += "\n<th>" + datos.get(x)[i] + "</th>";
                     }
-                }
-                else if (i == 5) {
+                } else if (i == 5) {
                     if (x > 0) {
                         html += "<td>" + datos.get(x)[i + 2] + "/" + datos.get(x)[i + 1] + "/" + datos.get(x)[i] + "</td>";
                     } else {
